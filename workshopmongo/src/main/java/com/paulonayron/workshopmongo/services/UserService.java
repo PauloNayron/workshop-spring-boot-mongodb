1140ff883abc51bbf3601a8e03b8b919dd7ccb73
PauloNayron <paulonayron.s@hotmail.com>
2019-06-27 22:58:27 -0300

    feat(User): implementação de Repository e Service da classe User e conectando o projeto ao MongoDB

---
+++ workshopmongo/src/main/java/com/paulonayron/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.paulonayron.workshopmongo.services;
+
+import com.paulonayron.workshopmongo.domain.User;
+import com.paulonayron.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
