4e53f68f4d2eff344715e5527050b3206771aa3b
PauloNayron <paulonayron.s@hotmail.com>
2019-07-01 21:53:02 -0300

    feat(UserResource): implementação de Endpoint para retornar os posts associados a um usuário

--- workshopmongo/src/main/java/com/paulonayron/workshopmongo/resources/UserResource.java
+++ workshopmongo/src/main/java/com/paulonayron/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.paulonayron.workshopmongo.resources;
 
+import com.paulonayron.workshopmongo.domain.Post;
 import com.paulonayron.workshopmongo.domain.User;
 import com.paulonayron.workshopmongo.dto.UserDTO;
 import com.paulonayron.workshopmongo.services.UserService;
@@ -52,6 +53,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 }
