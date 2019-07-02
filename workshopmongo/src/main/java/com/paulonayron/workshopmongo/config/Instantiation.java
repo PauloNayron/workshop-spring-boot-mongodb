4cbf8c73d364721bd043af354859987862db374f
PauloNayron <paulonayron.s@hotmail.com>
2019-07-01 21:15:44 -0300

    feat(User): Implementação do Domain e Reposity da Entity Post e inicialização de dados

--- workshopmongo/src/main/java/com/paulonayron/workshopmongo/config/Instantiation.java
+++ workshopmongo/src/main/java/com/paulonayron/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.paulonayron.workshopmongo.config;
 
+import com.paulonayron.workshopmongo.domain.Post;
 import com.paulonayron.workshopmongo.domain.User;
+import com.paulonayron.workshopmongo.repository.PostRepository;
 import com.paulonayron.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
@@ -14,15 +18,27 @@ public class Instantiation implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+
+        Post post1 = new Post(null, sdf.parse("01/06/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+        Post post2 = new Post(null, sdf.parse("29/05/2018"), "Bom dia", "Acordei Feliz hoje!", maria);
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
